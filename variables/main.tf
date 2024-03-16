fd1a7b567245066461d8c82284d445d9344966cd
David Davila <daviddavila@Davids-MacBook-Pro.local>
2024-03-15 21:48:54 -0230

    Learned how to create a list

--- variables/main.tf
+++ variables/main.tf
@@ -3,8 +3,16 @@ variable "vpcname" {
     default = "myvpc"
 }
 
-
 variable "sshport" {
     type = number
     default = 22
+}
+
+variable "enabled" {
+    default = true
+}
+
+variable "mylist" {
+  type = list(string)
+  default = [ "Value1", "Value2" ]
 }
